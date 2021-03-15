b23bde035718542ea90155fa867d2efd07e4c842
Alicia Weenum <weenum.alicia@gmail.com>
2021-03-15 12:16:27 -0400

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+    def index
+        users = User.all
+
+        render json: users.to_json(except: [:created_at, :updated_at, :category_id])
+    
+    end
+
+end
